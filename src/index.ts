629b17e7d464e0c8197d34e3cf8dab2c0e8a46f3
ppplllil-spec <banseun36@gmail.com>
2026-02-09 00:24:42 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import { jsxRenderer } from 'hono/jsx-renderer'
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import votes from './routes/votes'
+import schedule from './routes/schedule'
 
-export const renderer = jsxRenderer(({ children }) => {
-  return (
-    <html>
-      <head>
-        <link href="/static/style.css" rel="stylesheet" />
-      </head>
-      <body>{children}</body>
-    </html>
-  )
-})
+const app = new Hono()
+app.use('/api/*', cors()) // 모든 접속 허용
+
+// 주소 약속: 이 주소로 들어오면 각 파일로 연결해줍니다.
+app.route('/api/votes', votes)
+app.route('/api/schedule', schedule)
+
+export default app
